56317657b8e534576c57f01ea17fa53e12d995a6
chethan <chethan@chethan-PC>
2014-10-03 23:36:30 +0530

    New Widget class added

---
+++ DemoSlidingDrawer/src/it/sephiroth/demo/slider/widget/ButtonCustom.java
@@ -0,0 +1,14 @@
+package it.sephiroth.demo.slider.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class ButtonCustom extends Button {
+
+	public ButtonCustom(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+}
