867a8f978232a8d87e73d373ad6e5c496b038f4e
chethan <chethan@chethan-PC>
2014-10-03 23:40:24 +0530

    Feature class added to the file

---
+++ DemoSlidingDrawer/src/it/sephiroth/demo/feature/Feature.java
@@ -0,0 +1,12 @@
+package it.sephiroth.demo.feature;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Feature extends Activity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+}
+}
